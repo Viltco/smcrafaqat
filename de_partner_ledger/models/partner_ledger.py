2b6539b52bba9874e4b78e6b59e84b6fc50e683c
smcviltco <75363152+smcviltco@users.noreply.github.com>
2022-03-25 12:10:45 +0500

    all

--- de_partner_ledger/models/partner_ledger.py
+++ de_partner_ledger/models/partner_ledger.py
@@ -23,17 +23,8 @@ class PartnerLedger(models.TransientModel):
     partner_id = fields.Many2one('res.partner', string='Partner', required=True, help='Select Partner for movement')
 
     def print_report(self):
-        data = {'partner_id': self.partner_id.id,
-                'start_date': self.start_date,
-                'end_date': self.end_date}
+        data = {'partner_id': self.partner_id.id, 'start_date': self.start_date, 'end_date': self.end_date}
         return self.env.ref('de_partner_ledger.partner_ledger_pdf').report_action(self, data)
-
-    def hic_report(self):
-        print("uclick")
-        data = {'partner_id': self.partner_id.id,
-                'start_date': self.start_date,
-                'end_date': self.end_date}
-        return self.env.ref('de_partner_ledger.partner_ledger_hic_pdf').report_action(self, data)
 
 
 class ResPartner(models.Model):
--- de_partner_ledger/report/__init__.py
+++ de_partner_ledger/report/__init__.py
@@ -1,3 +1,2 @@
 # -*- coding: utf-8 -*-
 from . import partner_ledger_report
-from . import partner_ledger_hic_report
